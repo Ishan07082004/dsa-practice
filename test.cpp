ac2c85531c5cc62854f3d9191df314a361accb2e
Ishan Verma <ishanv002@gmail.com>
2025-01-04 23:37:59 +0530

    first code

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // solution comes here
+  cout << "This file will contain all the basic understanding of the algorithms and theorams that I'm practicing about data structures and algorithms.";
+  cout << endl;
+
+  cout << "Also one more thing is that this file will contain the questions that I will practice for competitive programming and other contests such as leetcode and codeforces";
+  return 0;
+
+}
