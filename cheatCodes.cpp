9524a0eab495827b634d73c1058ce41c5f99bfdc
Ishan Verma <ishanv002@gmail.com>
2025-01-04 23:54:43 +0530

    Cheat Codes of CP

---
+++ cheatCodes.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+// used for shortening the code
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+int main() {
+  ll a = 1234456676;
+  ll b = 9876543234;
+  cout << a * b << endl;
+}
