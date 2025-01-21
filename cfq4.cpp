afcae79320b6bcc8f1a31c04b3d05808862925df
Ishan Verma <ishanv002@gmail.com>
2025-01-21 15:05:06 +0530

    done!

---
+++ cfq4.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long long ll;
+// #define PB push_back
+int main() {
+  int n;
+  cin >> n;
+  for(int i = 1; i <= 12; i++) {
+    cout << n << " * " << i << " = " << n * i << endl;
+  }
+  return 0;
+}
