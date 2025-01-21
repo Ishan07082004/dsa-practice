f662b7ef5cdca1f3656f85d9887f3b1dabf9a777
Ishan Verma <ishanv002@gmail.com>
2025-01-21 14:50:05 +0530

    Question completed

---
+++ cfQ2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long long ll;
+// #define PB push_back
+int main() {
+  int n;
+  while(cin >> n) {
+    if(n == 1999) {
+      cout << "Correct" << endl;
+      break;
+    } else {
+      cout << "Wrong" << endl;
+    }
+  }
+  return 0;
+}
