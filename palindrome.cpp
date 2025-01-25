3cba84960661a1c4fead07f7391fae575cbca488
Ishan Verma <ishanv002@gmail.com>
2025-01-25 14:11:06 +0530

    solved!

---
+++ palindrome.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long long ll;
+// #define PB push_back
+bool palindrome(int i, string &s) {
+  if(i >= s.size() - 1) return true;
+  if(s[i] != s[s.size()-i-1]) return false;
+  return palindrome(i+1,s);
+}
+
+int main() {
+  string s = "madam";
+  cout << palindrome(0,s);
+  return 0;
+}
