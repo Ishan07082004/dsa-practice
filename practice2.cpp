5e73feb9ddd97761d43c7f96356f2b5b3c36fd2e
Ishan Verma <ishanv002@gmail.com>
2025-01-27 15:15:44 +0530

    new question done

---
+++ practice2.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long long ll;
+// #define PB push_back
+int main() {
+  cout << "successfully done revision till basic level" << endl;
+  cout << "moving to sorting algorithms now";
+  return 0;
+}
