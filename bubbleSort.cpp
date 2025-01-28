f91b76f10329cd504d8dd1dfbc91fb36871a6437
Ishan Verma <ishanv002@gmail.com>
2025-01-28 12:16:55 +0530

    few additions

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,12 +5,17 @@ using namespace std;
 
 void bubbleSort(int arr[], int n) {
   for(int i=n-1; i>=0; i--) {
+    int didSwap = 0;
     for(int j=0; j <= i-1; j++) {
       if(arr[j] > arr[j+1]) {
         int temp = arr[j+1];
         arr[j+1] = arr[j];
         arr[j] = temp;
+        didSwap = i;
       }
+    }
+    if(didSwap == 0) {
+      break;
     }
   }
 }
