ef4f021ab0c79dda5ba62178f3ae6c8907d8a3d8
Ishan Verma <ishanv002@gmail.com>
2025-01-28 11:39:54 +0530

    completed selection sort revision

---
+++ selectionSort.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long long ll;
+// #define PB push_back
+
+void selectionSort(int arr[], int n) {
+  for(int i=0; i<=n-2; i++) {
+    int mini = i;
+    for(int j=1; j<=n-1; j++) {
+      if(arr[j] < arr[mini]) {
+        mini = j;
+      }
+    }
+    int temp;
+    temp = arr[mini];
+    arr[mini] = arr[i];
+    arr[i] = temp;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i=0; i<n; i++) cin >> arr[i];
+  selectionSort(arr, n);
+  for(int i=0; i<n; i++) {
+    cout << arr[i] << " ";
+  }
+  return 0;
+}
