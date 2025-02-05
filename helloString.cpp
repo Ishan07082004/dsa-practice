92b3eab0ec04e95055041944619c362f108db738
Ishan Verma <ishanv002@gmail.com>
2025-02-05 23:57:33 +0530

    done

---
+++ helloString.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long long ll;
+// #define PB push_back
+int main() {
+  int n = 5;
+  int k;
+  cout << "Enter the value: ";
+  cin >> k;
+  int j = k*k + n;
+  cout << "Final answer is: " << j;
+  return 0;
+}
